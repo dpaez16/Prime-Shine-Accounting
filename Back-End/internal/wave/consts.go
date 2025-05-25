daffca862091c0ca1b127b009a1c12d43c93dde4
Danny Paez <dpaez16@yahoo.com>
2025-05-25 11:29:12 -0700

    Moved env variables to consts

---
+++ Back-End/internal/wave/consts.go
@@ -0,0 +1,8 @@
+package wave
+
+const (
+	WAVE_CLEANING_PRODUCT_NAME = "Cleaning"
+	WAVE_BUSINESS_NAME         = "Prime Shine Cleaning Services, Inc."
+	WAVE_BUSINESS_URL          = "https://api.waveapps.com/businesses"
+	WAVE_GRAPHQL_URL           = "https://gql.waveapps.com/graphql/public"
+)
