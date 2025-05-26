8774f10dfdac855fd5e122dd3eefd344e81c3c2f
Danny Paez <dpaez16@yahoo.com>
2025-05-25 18:27:29 -0700

    Corrected bootstrap script

---
+++ Back-End/internal/data/postgres.go
@@ -0,0 +1,22 @@
+package data
+
+import (
+	"os"
+
+	"github.com/jackc/pgx"
+	"github.com/pkg/errors"
+)
+
+func ConnectDB_PG() (*pgx.Conn, error) {
+	connection, err := pgx.Connect(pgx.ConnConfig{
+		User:     os.Getenv("POSTGRES_USER"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		Host:     os.Getenv("POSTGRES_HOST"),
+	})
+
+	if err != nil {
+		return nil, errors.Wrap(err, "pgx.Connect")
+	}
+
+	return connection, nil
+}
--- Docker/dev/postgres/bootstrap.sh
+++ Docker/dev/postgres/bootstrap.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f tables.ddl
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f data.ddl
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/tables.ddl
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /docker-entrypoint-initdb.d/data.ddl
