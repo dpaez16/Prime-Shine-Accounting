7cc49f0ad19baa905375a9eed59dc6b5513a456f
Danny Paez <dpaez16@yahoo.com>
2025-05-27 10:16:58 -0700

    Clean up

--- Front-End/src/components/schedulesPage/schedulesPage.tsx
+++ Front-End/src/components/schedulesPage/schedulesPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Table, Message } from 'semantic-ui-react';
 import CreateScheduleModal from './createScheduleModal/createScheduleModal';
@@ -8,134 +8,89 @@ import PrimeShineAPIClient from '../../api/primeShineApiClient';
 import useLocalization from '../../hooks/useLocalization';
 import LoadingSegment from '../loadingSegment/loadingSegment';
 import { dateToStr } from '../../utils/helpers';
-import { v4 as uuidV4 } from 'uuid';
-import { Schedule, ScheduleID } from '@/types/schedule';
+import { ScheduleID } from '@/types/schedule';
 import { LoginSessionContext } from '@/context/LoginSessionContext';
+import { useDataFetcher } from '@/hooks/useDataFetcher';
 
 export default function SchedulesPage() {
-  const context = useContext(LoginSessionContext);
-  const userInfo = context.userInfo!;
+    const context = useContext(LoginSessionContext);
+    const userInfo = context.userInfo!;
 
-  const [schedules, setSchedules] = useState<Schedule[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const { t } = useLocalization();
-  const navigate = useNavigate();
+    const { data, loading, error, refetch } = useDataFetcher({ fetcher: () => PrimeShineAPIClient.fetchSchedules(userInfo._id, userInfo.token) });
+    const { t } = useLocalization();
+    const navigate = useNavigate();
 
-  const createScheduleHandler = (startDay: Date) => {
-    const userId = userInfo._id;
-    const jwt = userInfo.token;
+    const createScheduleHandler = (startDay: Date) => {
+        const userId = userInfo._id;
+        const jwt = userInfo.token;
 
-    PrimeShineAPIClient.createSchedule(startDay, userId, jwt)
-      .then((schedule) => {
-        setSchedules([...schedules, schedule]);
-        setError(null);
-      })
-      .catch((err) => {
-        setError(err.message);
-      });
+        PrimeShineAPIClient.createSchedule(startDay, userId, jwt)
+            .then(() => refetch())
+            .catch((err) => alert('Failed to create schedule: ' + err.message)); // TODO: use translation hook
+    };
+
+    const editScheduleHandler = (startDay: Date, scheduleId: ScheduleID) => {
+        const jwt = userInfo.token;
+
+        PrimeShineAPIClient.editSchedule(startDay, scheduleId, jwt)
+            .then(() => refetch())
+            .catch((err) => alert('Failed to edit schedule: ' + err.message)); // TODO: use translation hook
   };
 
-  const editScheduleHandler = (startDay: Date, scheduleId: ScheduleID) => {
-    const jwt = userInfo.token;
+    const deleteScheduleHandler = (scheduleID: ScheduleID) => {
+        const jwt = userInfo.token;
 
-    PrimeShineAPIClient.editSchedule(startDay, scheduleId, jwt)
-      .then((patchedSchedule) => {
-        const newSchedules = [...schedules];
-        const idx = newSchedules.findIndex(
-          (schedule) => schedule._id === scheduleId,
-        );
-        newSchedules.splice(idx, 1, patchedSchedule);
+        PrimeShineAPIClient.deleteSchedule(scheduleID, jwt)
+            .then(() => refetch())
+            .catch((err) => alert('Failed to delete schedule: ' + err.message)); // TODO: use translation hook
+    };
 
-        setSchedules(newSchedules);
-        setError(null);
-      })
-      .catch((err) => {
-        setError(err.message);
-      });
-  };
+    const schedules = data ?? [];
+    const sortedSchedules = schedules.sort((a, b) => a.startDay.getTime() - b.startDay.getTime());
 
-  const deleteScheduleHandler = (scheduleID: ScheduleID) => {
-    const jwt = userInfo.token;
-
-    PrimeShineAPIClient.deleteSchedule(scheduleID, jwt)
-      .then(() => {
-        const newSchedules = [...schedules];
-        const idx = newSchedules.findIndex(
-          (schedule) => schedule._id === scheduleID,
-        );
-        newSchedules.splice(idx, 1);
-
-        setSchedules(newSchedules);
-        setError(null);
-      })
-      .catch((err) => {
-        setError(err.message);
-      });
-  };
-
-  useEffect(() => {
-    PrimeShineAPIClient.fetchSchedules(userInfo._id, userInfo.token)
-      .then((fetchedSchedules) => {
-        setSchedules(fetchedSchedules);
-        setLoading(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setLoading(false);
-        setError(err.message);
-      });
-  }, []);
-
-  const sortedSchedules = schedules.sort((a, b) => a.startDay.getTime() - b.startDay.getTime());
-
-  return (
-    <div className='flex flex-col mx-auto w-1/2'>
-      <h1>{t('Schedules')}:</h1>
-      {loading && <LoadingSegment />}
-      <div>
-        <CreateScheduleModal
-          onSubmit={(startDate) => {
-            createScheduleHandler(startDate);
-          }}
-        />
-      </div>
-      {error && <Message negative content={error} />}
-      <Table celled>
-        <Table.Body>
-          {sortedSchedules.map((schedule) => {
-            return (
-              <Table.Row key={uuidV4()}>
-                <Table.Cell className='flex flex-row justify-between items-center'>
-                  <a
-                    href='/viewSchedule'
-                    onClick={(e) => {
-                      e.preventDefault();
-                      navigate('/viewSchedule', {
-                        state: {
-                          schedule: schedule,
-                        },
-                      });
-                    }}
-                  >
-                    {dateToStr(schedule.startDay)}
-                  </a>
-                  <div className='flex flex-row gap-2'>
-                    <EditScheduleModal
-                      schedule={schedule}
-                      onSubmit={(startDay) => editScheduleHandler(startDay, schedule._id)}
-                    />
-                    <DeleteScheduleModal
-                      startDay={dateToStr(schedule.startDay)}
-                      onSubmit={() => deleteScheduleHandler(schedule._id)}
-                    />
-                  </div>
-                </Table.Cell>
-              </Table.Row>
-            );
-          })}
-        </Table.Body>
-      </Table>
-    </div>
-  );
+    return (
+        <div className='flex flex-col mx-auto w-1/2'>
+            <h1>{t('Schedules')}:</h1>
+            {loading && <LoadingSegment />}
+            <div>
+                <CreateScheduleModal onSubmit={createScheduleHandler} />
+            </div>
+            {error && <Message negative content={error.message} />}
+            <Table celled>
+                <Table.Body>
+                {sortedSchedules.map((schedule, idx) => {
+                    return (
+                        <Table.Row key={idx}>
+                            <Table.Cell className='flex flex-row justify-between items-center'>
+                                <a
+                                    href='/viewSchedule'
+                                    onClick={(e) => {
+                                        e.preventDefault();
+                                        navigate('/viewSchedule', {
+                                            state: {
+                                                schedule: schedule,
+                                            },
+                                        });
+                                    }}
+                                >
+                                    {dateToStr(schedule.startDay)}
+                                </a>
+                                <div className='flex flex-row gap-2'>
+                                    <EditScheduleModal
+                                        schedule={schedule}
+                                        onSubmit={(startDay) => editScheduleHandler(startDay, schedule._id)}
+                                    />
+                                    <DeleteScheduleModal
+                                        startDay={dateToStr(schedule.startDay)}
+                                        onSubmit={() => deleteScheduleHandler(schedule._id)}
+                                    />
+                                </div>
+                            </Table.Cell>
+                        </Table.Row>
+                    );
+                })}
+                </Table.Body>
+            </Table>
+        </div>
+    );
 }
