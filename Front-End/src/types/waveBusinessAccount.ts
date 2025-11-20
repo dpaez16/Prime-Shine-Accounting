85a52f31718bd4045d6a2045f79ac236c6191ef2
Danny Paez <dpaez16@yahoo.com>
2025-11-19 18:21:10 -0800

    Filled out more of WaveBusinessAccount type

--- Front-End/src/types/waveBusinessAccount.ts
+++ Front-End/src/types/waveBusinessAccount.ts
@@ -1,6 +1,30 @@
+export type WaveBusinessAccountGroup = {
+    value: number;
+    display_name: string;
+};
+
+export type WaveBusinessAccountSubGroup = {
+    value: number;
+    display_name: string;
+};
+
+export type WaveBusinessAccountAccrualAnchorTier = {
+    value: number;
+    display_name: string;
+};
+
+export type WaveBusinessAccountNormalBalanceType = {
+    value: number;
+    display_name: string;
+};
+
 export type WaveBusinessAccount = {
     archived: boolean;
     active: boolean;
     account_name: string;
     wave_classic_pk: number;
+    group: WaveBusinessAccountGroup;
+    sub_group: WaveBusinessAccountSubGroup;
+    accrual_anchor_tier: WaveBusinessAccountAccrualAnchorTier;
+    normal_balance_type: WaveBusinessAccountNormalBalanceType;
 };
